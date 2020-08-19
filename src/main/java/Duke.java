be7fa4d19ff29a95caf55692c57561e3ca38ca96
R Ramana <e0398581@u.nus.edu>
2020-08-19 22:16:13 +0800

    Add Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        String logo = " ____   ____    _____   _____   _    _  \n"
+                + "|  __| |  _ \\  |_   _| |_   _| | |  | | \n"
+                + "| |__  | | | |   | |     | |   | |__| | \n"
+                + "|  __| | | | |   | |     | |   |  __  | \n"
+                + "| |__  | |_| |  _| |_    | |   | |  | | \n"
+                + "|____| |____/  |_____|   |_|   |_|  |_| \n";
+        System.out.println(logo);
 
         String line = "____________________________________________________________\n";
-        String greeting = " Hello! I'm Duke\n" +
-                " What can I do for you?\n";
-        String bye = " Bye. Hope to see you again soon!\n";
+        String greeting = line +
+                " Hello, I'm Edith!\n" +
+                " I am a chatbot and I am here to assist you.\n" +
+                " What can I help you with?\n" +
+                line;
 
-        System.out.println(line + greeting + line + bye + line);
+        System.out.println(greeting);
+
+        // Create scanner class to take in new user inputs
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println(line + ' ' + userInput + '\n' + line);
+            userInput = input.nextLine();
+        }
+
+        String bye = line + " Bye. I hope I have helped you. See you soon!\n" + line;
+        System.out.println(bye);
+
     }
 }
