37700a827b1a6339909c05ff522e906ec079f2b0
R Ramana <e0398581@u.nus.edu>
2020-08-26 12:52:08 +0800

    Add Level 2. List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____   ____    _____   _____   _    _  \n"
-                + "|  __| |  _ \\  |_   _| |_   _| | |  | | \n"
-                + "| |__  | | | |   | |     | |   | |__| | \n"
-                + "|  __| | | | |   | |     | |   |  __  | \n"
-                + "| |__  | |_| |  _| |_    | |   | |  | | \n"
-                + "|____| |____/  |_____|   |_|   |_|  |_| \n";
+        String logo = " ____   ____    _____   _____   _    _  " + System.lineSeparator()
+                + "|  __| |  _ \\  |_   _| |_   _| | |  | | " + System.lineSeparator()
+                + "| |__  | | | |   | |     | |   | |__| | " + System.lineSeparator()
+                + "|  __| | | | |   | |     | |   |  __  | " + System.lineSeparator()
+                + "| |__  | |_| |  _| |_    | |   | |  | | " + System.lineSeparator()
+                + "|____| |____/  |_____|   |_|   |_|  |_| ";
         System.out.println(logo);
 
         String line = "____________________________________________________________\n";
@@ -23,8 +23,26 @@ public class Duke {
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
 
+        // Creating a task list
+        String[] toDoList = new String[100];
+        int taskNumber = 0;
+
         while (!userInput.equals("bye")) {
-            System.out.println(line + ' ' + userInput + '\n' + line);
+            //Add to do list
+            if(!userInput.equals("list")) {
+                toDoList[taskNumber] = userInput;
+                taskNumber++;
+                System.out.println(line + "\n added: " + userInput + '\n' + line);
+            }
+            // Print out to do list
+            if(userInput.equals("list") || userInput.equals("List")) {
+                System.out.println(line);
+                for (int i = 1; i <= taskNumber; i++) {
+                    System.out.println(" " + i + ". " + toDoList[i-1] );
+                }
+                System.out.println(line);
+            }
+
             userInput = input.nextLine();
         }
 
